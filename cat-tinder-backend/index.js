378470fe3cc8d59b54506a4883bab1093267e613
Alyssa Hope <alyssa@organikseo.com>
2018-02-21 12:40:47 -0800

    ...

--- cat-tinder-backend/index.js
+++ cat-tinder-backend/index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 const PORT = process.env.PORT || 3000
 
-app.listen(3000, function () {
- console.log(' Server listening on ', 3000);
+app.listen(PORT, function () {
+ console.log(' Server listening on ', PORT);
 });
